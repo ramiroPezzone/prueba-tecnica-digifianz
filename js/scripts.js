0463f56a90ab4c6c0ddc64341a2e365468c5fc26
ramiroPezzone <ramiro.pezzone@gmail.com>
2022-02-24 10:57:55 -0300

    corrección del imageHero, corrección de tipos de inputs, agregado de direccionamiento a links del navbar y animación de cierre cuando se selecciona una opción

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,47 @@
-const todoElDocumento = document.querySelector('*');
-const capaOpaca = document.querySelector('.capa-opaca-oculta');
+const todoElDocumento = document.querySelector("*");
+const capaOpaca = document.querySelector(".capa-opaca-oculta");
 
-// Despliegue menú oculto
-if (document.querySelector('.menu-hamb')) {
-    const btnMenuHamb = document.querySelector('.menu-hamb');
-    const btnMenuHambClose = document.querySelector('.btn-cierre-menu');
-    const navOptions = document.querySelector('.nav-options');
+// Despliegue/ocultamiento menú oculto
+if (document.querySelector(".menu-hamb")) {
+  const btnMenuHamb = document.querySelector(".menu-hamb");
+  const btnMenuHambClose = document.querySelector(".btn-cierre-menu");
+  const navOptions = document.querySelector(".nav-options");
+  const aOptions = document.querySelectorAll(".a-option");
 
-    btnMenuHamb.addEventListener('click', () => {
-        navOptions.classList.add('nav-options-revealed');
-        capaOpaca.classList.add('capa-opaca-revealed')
-        todoElDocumento.style.overflowY = 'hidden';
-    })
-    btnMenuHambClose.addEventListener('click', () => {
-        navOptions.classList.remove('nav-options-revealed')
-        capaOpaca.classList.remove('capa-opaca-revealed')
-        todoElDocumento.style.overflowY = 'initial'
-    })
+  btnMenuHamb.addEventListener("click", () => {
+    navOptions.classList.add("nav-options-revealed");
+    capaOpaca.classList.add("capa-opaca-revealed");
+    todoElDocumento.style.overflowY = "hidden";
+  });
+  btnMenuHambClose.addEventListener("click", () => {
+    navOptions.classList.remove("nav-options-revealed");
+    capaOpaca.classList.remove("capa-opaca-revealed");
+    todoElDocumento.style.overflowY = "initial";
+  });
+  aOptions.forEach((opt) => {
+    opt.addEventListener("click", () => {
+      navOptions.classList.remove("nav-options-revealed");
+      capaOpaca.classList.remove("capa-opaca-revealed");
+      todoElDocumento.style.overflowY = "initial";
+    });
+  });
 }
 
 // Form oculto
-if (document.querySelector('.contenedor-form-hidden')) {
-    const btnLg = document.querySelector('.btn-lg');
-    const formOculto = document.querySelector('.contenedor-form-hidden');
-    const cruzCierreForm = document.querySelector('.cruz-cierre-form');
-    
-    btnLg.addEventListener('click', () => {
-        formOculto.classList.add('contenedor-form-revealed')
-        capaOpaca.classList.add('capa-opaca-revealed')
-        todoElDocumento.style.overflowY = 'hidden';
-    })
-    
-    cruzCierreForm.addEventListener('click', () => {
-        formOculto.classList.remove('contenedor-form-revealed')
-        capaOpaca.classList.remove('capa-opaca-revealed')
-        todoElDocumento.style.overflowY = 'initial'
-    })
+if (document.querySelector(".contenedor-form-hidden")) {
+  const btnLg = document.querySelector(".btn-lg");
+  const formOculto = document.querySelector(".contenedor-form-hidden");
+  const cruzCierreForm = document.querySelector(".cruz-cierre-form");
 
+  btnLg.addEventListener("click", () => {
+    formOculto.classList.add("contenedor-form-revealed");
+    capaOpaca.classList.add("capa-opaca-revealed");
+    todoElDocumento.style.overflowY = "hidden";
+  });
+
+  cruzCierreForm.addEventListener("click", () => {
+    formOculto.classList.remove("contenedor-form-revealed");
+    capaOpaca.classList.remove("capa-opaca-revealed");
+    todoElDocumento.style.overflowY = "initial";
+  });
 }
